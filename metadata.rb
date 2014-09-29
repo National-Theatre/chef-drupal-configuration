899460351a593ef986d610dc0db7ff5fcd3507ed
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2014-09-29 14:43:48 +0100

    fix version deps

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'All rights reserved'
 description      'Installs/Configures drupal-configuration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
-depends          'drush', '>=0.10.0'
+depends          'drush', '>= 0.10.0'
